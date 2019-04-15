7c0aae12d3e155629c833f90da67919b1ec64bfa
Amaury MAILLE <amaury@mi-lip-amaury.lip.ens-lyon.fr>
2019-04-15 10:14:03 +0200

    Start reworking installation script for Debian.
      * Implement functions to check for version of GHC, LLDB and Cabal
      * After building PCRE, go back to the root directory of Encore before making Encore

--- debian-install.sh
+++ debian-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -e -x
 quiet=''
 replace='false'
 set_path='false'
@@ -19,7 +19,13 @@ while getopts 'qrbphf' flag; do
             echo "-s  Adds Encore to the \$PATH variable. Has no effect unless the \`-b\` flag is used.";
             echo "-b  Builds Encore.";
             echo "-h  Print this message and exit.";
-            echo "-f  Perform a full install. Equivalent to \`-rsb\`";exit;;
+            echo "-f  Perform a full install. Equivalent to \`-rsb\`";
+            echo ""
+            echo "Available environment variable to configure build are:";
+            echo "ENCORE_LLDB_PKG   Specify the name of the LLDB package to install. Default: lldb-3.5";
+            echo "ENCORE_GHC_PKG    Specify the name of the GHC package to install. Default: ghc-7.10.3";
+            echo "ENCORE_CABAL_PKG  Specify the name of the Cabal package to install. Default: cabal-install-1.22"; exit ;;
+            
     *) echo "Unexpected option ${flag}. Use \`-h\` to see available options"; exit ;;
   esac
 done
@@ -29,6 +35,58 @@ sudo add-apt-repository -y ppa:hvr/ghc
 
 echo "--== UPDATING REPOS ==--"
 sudo apt-get update $quiet
+
+echo "--== CHECKING VERSIONS ==--"
+check_version() 
+{
+    local APP=$1
+    local PKG=$2
+    local VERSION_CHECK="$3"
+    local EXPECTED_VERSION=$4
+    
+    local APP_PATH=$(which "$APP")
+    if [[ -z "$APP_PATH" ]]
+    then
+        echo "Info: No installation of $APP found"
+        if [[ $(apt search -n $PKG 2>/dev/null | wc -l) -eq 2 ]]
+        then
+            echo "No candidate package for $APP found. Please install $APP yourself"
+            exit
+        else
+            return 1
+        fi
+    else
+        local VERSION=$($VERSION_CHECK)
+        if [[ ! "$VERSION" == "$EXPECTED_VERSION" ]]
+        then
+            echo "Warning: Found $APP version $VERSION. Encore expects version $EXPECTED_VERSION. Errors may occur."
+        else
+            echo "Info: Found $APP version $VERSION"
+        fi
+    fi
+
+    return 0
+}
+
+check_ghc_version()
+{
+    ghc --version | grep -o "version .*" | sed -e 's/version //g'
+}
+
+check_cabal_version()
+{
+    cabal --version | head -n 1 | grep -o "version [0-9]*\.[0-9]*" | sed -e 's/version //g'
+}
+
+check_lldb_version()
+{
+    lldb --version | grep -o "version [0-9]*.[0-9]*" | sed -e 
+}
+
+if [[ ! $(check_version "ghc" "ghc-7.10.3" check_ghc_version "7.10.3") ]]
+then
+    GHC_PKG="ghc-7.10.3"
+fi
 
 echo "--== INSTALLING STUFF ==--"
 sudo apt-get install -y $quiet\
@@ -47,6 +105,9 @@ tar xvf pcre2-10.21.tar.bz2
 cd pcre2-10.21
 ./configure --prefix=/usr
 make
+
+cd ..
+
 sudo make install
 
 if $replace
